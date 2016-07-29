b2b9832ac14f07b563db0f0beee64210a0cf971b
mrpotatosotong <62390@rp.edu.sg>
2016-07-30 00:23:11 +0800

    by tl
    removed unneeded files
    adding documentations

--- getLatLong-1.py
+++
@@ -1,8 +0,0 @@
-#import geocoder
-#g = geocoder.google('Singapore')
-#g.latlng
-from geopy.geocoders import Nominatim
-
-geolocator = Nominatim()
-location = geolocator.geocode("Victoria SG")
-print(location.latitude, location.longitude)
